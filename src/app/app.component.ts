40feb137eed7138f0a6261d5ae48c5d7b23c047e
nile <nile.dx@gmail.com>
2020-10-05 20:18:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'rgi-app';
+  isMobile: boolean;
+  constructor() {
+    if (window.innerWidth < 1024) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
+
+  onResize(e) {
+    if (e && e.target.innerWidth > 1025) {
+      this.isMobile = false;
+    }
+    if (e && e.target.innerWidth < 1024){
+      this.isMobile = true;
+    }
+    return this.isMobile;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { JumbotronComponent } from './shared/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
